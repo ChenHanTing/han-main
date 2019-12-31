5eac4fe94efb689fdb1136a341b3cc0c2e622618
陳漢庭 <han@jvdiamondtech.com>
2019-12-31 14:10:35 +0800

    add the rest of useless stuff

--- test/models/lab_forum_test.rb
+++ test/models/lab_forum_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: lab_forums
+#
+#  id          :bigint           not null, primary key
+#  category    :string(255)
+#  click       :integer          default(0)
+#  description :text(65535)
+#  question    :string(255)
+#  solved      :boolean          default(FALSE)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
+
 require 'test_helper'
 
 class LabForumTest < ActiveSupport::TestCase
