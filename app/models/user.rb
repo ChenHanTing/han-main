72635dfa5b91c85f5bdd8ea1598d98f22bc0660e
Han <han@jvdiamondtech.com>
2020-01-06 16:54:59 +0800

    chatroom CRUD

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,4 +80,10 @@ class User < ApplicationRecord
 
     token
   end
+
+  def gravatar_url
+    gravatar_id = Digest::MD5::hexdigest(email).downcase
+
+    "https://gravatar.com/avatar/#{gravatar_id}.png"
+  end
 end
