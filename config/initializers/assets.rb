429f33e272756d06d28c86661d2d9f7ea4033f5a
Han <han@jvdiamondtech.com>
2020-01-11 09:49:56 +0800

    modify resume

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,4 +16,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w[ckeditor/config.js
                                                  card.css
                                                  testimonial.css
+                                                 resume.css
                                                 ]
